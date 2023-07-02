db8c493b39a99570f08caeded567ce53bbc8b43f
cphung913 <chasephung13@gmail.com>
2023-07-02 00:16:36 -0700

    added delete and keyboard functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const display = document.querySelector(".display");
 const current = display.querySelector(".current");
 
 const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
 
 const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
@@ -17,9 +18,51 @@ operations.forEach(op => {
     op.addEventListener('click', addOperation)
 })
 clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', backspace);
+
+addEventListener('keydown', keys);
+
+function keys(e) {
+    if (e.code.slice(0, -1) == 'Digit' && !e.shiftKey || e.key == ".") {
+        let num = e.key;
+        if (currentText.length >= 11) return;
+        if (num == ".") {
+            if (isUsingDecimal) return;
+            isUsingDecimal = true;
+        }
+    currentText = currentText.concat(num);
+    setCurrentText(currentText);
+    }
+    if (e.key == "+" || e.key == "-" || e.key == "=" || e.key == "x" || e.key == "/" || e.key == "*" || e.key == "Enter") {
+        let op = e.key;
+        if (op == "/") op = "÷";
+        if (op == "*") op = "x";
+        if (op == "Enter") op = "=";
+        if (op == "=" || isOperating) {
+            const equationArray = currentText.split(" ");
+            if (!equationArray[2]) return;
+            let newNum = operate(equationArray[0], equationArray[1], equationArray[2]);
+            if (newNum % 1 != 0) {
+                let toShave = newNum.toString().indexOf(".");
+                newNum = newNum.toFixed(10 - toShave);
+                let isZero = newNum.toString().slice(-1) == "0";
+                while (isZero) {
+                    newNum = newNum.slice(0, -1);
+                    isZero = newNum.toString().slice(-1) == "0";
+                }
+            }
+            setCurrentText("" + newNum);
+            currentText = "" + newNum;
+            return;
+        }
+        currentText = currentText.concat(" ", op, " ");
+        isOperating = true;
+        setCurrentText(currentText);
+    }
+}
 
 function addNumber(e) {
-    const num = e.target.textContent;
+    let num = e.target.textContent;
     if (currentText.length >= 11) return;
     if (num == ".") {
         if (isUsingDecimal) return;
@@ -38,7 +81,6 @@ function addOperation(e) {
         if (newNum % 1 != 0) {
             let toShave = newNum.toString().indexOf(".");
             newNum = newNum.toFixed(10 - toShave);
-            console.log(newNum.toString().slice(-1));
             let isZero = newNum.toString().slice(-1) == "0";
             while (isZero) {
                 newNum = newNum.slice(0, -1);
@@ -59,6 +101,12 @@ function clear() {
     setCurrentText("");
     isUsingDecimal = false;
     isOperating = false;
+}
+
+function backspace() {
+    if (currentText.slice(-1) == " ") currentText = currentText.slice(0, -1);
+    currentText = currentText.slice(0, -1);
+    setCurrentText(currentText);
 }
 
 function setCurrentText(a) {
